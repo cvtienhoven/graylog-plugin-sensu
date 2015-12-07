5765ab8935c7be8df5d5a48eeb414a1739297123
Christiaan van Tienhoven <tienhoven.c@tkppensioen.nl>
2015-12-07 10:45:03 +0100

    Corrected unit tests for JSON assertion

--- src/test/java/org/graylog/SensuResultFactoryTest.java
+++ src/test/java/org/graylog/SensuResultFactoryTest.java
@@ -2,6 +2,10 @@ package org.graylog;
 
 import static org.junit.Assert.*;
 
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
 import org.junit.Before;
 import org.junit.Test;
 
@@ -14,66 +18,71 @@ public class SensuResultFactoryTest {
 	}
 	
 	@Test
-	public void testCreateResultOneHandlerNoSubscribers(){
+	public void testCreateResultOneHandlerNoSubscribers() throws ParseException{
 		
 		String result = resultFactory.createResult("check_name", 1, "test_output", "handler", 123456789L, "client", "");
-		assertEquals("{\"check\":"
-				+ "{"
-				+ "\"executed\":123456789,"
-				+ "\"status\":1,"
-				+ "\"issued\":123456789,"
-				+ "\"name\":\"check_name\","
-				+ "\"command\":\"none\","
-				+ "\"publish\":false,"
-				+ "\"output\":\"test_output\","
-				+ "\"standalone\":true,"
-				+ "\"type\":\"standard\","
-				+ "\"handler\":\"handler\"},"
-				+ "\"client\":\"client\"}",
-				result
-				);
+				
+		JSONObject json = (JSONObject) new JSONParser().parse(result);
+		
+		JSONObject check = (JSONObject) json.get("check");
+		
+		assertEquals(123456789L, check.get("executed"));
+		assertEquals(1L, check.get("status"));
+		assertEquals(123456789L, check.get("issued"));
+		assertEquals("check_name", check.get("name"));
+		assertEquals(false, check.get("publish"));
+		assertEquals("test_output", check.get("output"));
+		assertEquals(true, check.get("standalone"));
+		assertEquals("standard", check.get("type"));
+		assertEquals("handler", check.get("handler"));
+		assertEquals("client", json.get("client"));
 	}
 	
 	@Test
-	public void testCreateResultMultipleHandlersOneSubscriber(){
+	public void testCreateResultMultipleHandlersOneSubscriber() throws ParseException{
 		String result = resultFactory.createResult("check_name ", 1, " test_output ", " handler1, handler2", 123456789L, "client ", "subscriber");
-		assertEquals("{\"check\":"
-				+ "{"
-				+ "\"executed\":123456789,"
-				+ "\"status\":1,"
-				+ "\"issued\":123456789,"
-				+ "\"name\":\"check_name\","
-				+ "\"command\":\"none\","
-				+ "\"publish\":false,"
-				+ "\"subscribers\":[\"subscriber\"],"
-				+ "\"output\":\"test_output\","
-				+ "\"standalone\":true,"
-				+ "\"type\":\"standard\","
-				+ "\"handlers\":[\"handler1\",\"handler2\"]},"
-				+ "\"client\":\"client\"}",
-				result
-				);
+		
+		JSONObject json = (JSONObject) new JSONParser().parse(result);
+		
+		JSONObject check = (JSONObject) json.get("check");
+		
+		assertEquals(123456789L, check.get("executed"));
+		assertEquals(1L, check.get("status"));
+		assertEquals(123456789L, check.get("issued"));
+		assertEquals("check_name", check.get("name"));
+		assertEquals(false, check.get("publish"));
+		assertEquals("test_output", check.get("output"));
+		assertEquals(true, check.get("standalone"));
+		assertEquals("standard", check.get("type"));
+		assertEquals("handler1", ((JSONArray) check.get("handlers")).get(0));
+		assertEquals("handler2", ((JSONArray) check.get("handlers")).get(1));
+		assertEquals("subscriber", ((JSONArray) check.get("subscribers")).get(0));
+		assertEquals("client", json.get("client"));
+
 	}
 	
 	@Test
-	public void testCreateResultMultipleSubscribers(){
+	public void testCreateResultMultipleSubscribers() throws ParseException{
 		String result = resultFactory.createResult("check_name ", 1, " test_output ", " handler1, handler2", 123456789L, "client ", "subscriber1, subscriber2");
-		assertEquals("{\"check\":"
-				+ "{"
-				+ "\"executed\":123456789,"
-				+ "\"status\":1,"
-				+ "\"issued\":123456789,"
-				+ "\"name\":\"check_name\","
-				+ "\"command\":\"none\","
-				+ "\"publish\":false,"
-				+ "\"subscribers\":[\"subscriber1\",\"subscriber2\"],"
-				+ "\"output\":\"test_output\","
-				+ "\"standalone\":true,"
-				+ "\"type\":\"standard\","
-				+ "\"handlers\":[\"handler1\",\"handler2\"]},"
-				+ "\"client\":\"client\"}",
-				result
-				);
+		
+		JSONObject json = (JSONObject) new JSONParser().parse(result);
+		
+		JSONObject check = (JSONObject) json.get("check");
+		
+		assertEquals(123456789L, check.get("executed"));
+		assertEquals(1L, check.get("status"));
+		assertEquals(123456789L, check.get("issued"));
+		assertEquals("check_name", check.get("name"));
+		assertEquals(false, check.get("publish"));
+		assertEquals("test_output", check.get("output"));
+		assertEquals(true, check.get("standalone"));
+		assertEquals("standard", check.get("type"));
+		assertEquals("handler1", ((JSONArray) check.get("handlers")).get(0));
+		assertEquals("handler2", ((JSONArray) check.get("handlers")).get(1));
+		assertEquals("subscriber1", ((JSONArray) check.get("subscribers")).get(0));
+		assertEquals("subscriber2", ((JSONArray) check.get("subscribers")).get(1));
+		assertEquals("client", json.get("client"));
+		
 	}
 }
 
